8362c93b6d45fe44c254b778e15389bd1b87b285
huzan2 <huzan2@naver.com>
2025-03-08 14:17:41 +0900

    init: 프로젝트 생성

---
+++ src/Routers.jsx
@@ -0,0 +1,15 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import MainPage from './Pages/MainPage';
+
+const Routers = () => {
+  const routes = [
+    {
+      path: '/',
+      element: <MainPage />,
+    },
+  ];
+  const router = createBrowserRouter(routes);
+  return <RouterProvider router={router} />;
+};
+
+export default Routers;
