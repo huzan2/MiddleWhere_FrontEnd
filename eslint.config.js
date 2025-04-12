25c3042cdb89fb7d6c60024226e5d920847519bb
huzan2 <huzan2@naver.com>
2025-04-13 04:57:04 +0900

    Reapply "chore: global style 및 theme 초기세팅"
    This reverts commit 1a2346b4ba3f4d8d90a59c9968911d13b5377cf0.

--- eslint.config.js
+++ eslint.config.js
@@ -16,11 +16,13 @@ export default [
         sourceType: 'module',
       },
     },
+    extends: ['plugin:react/recommended', 'plugin:prettier/recommended'],
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
+      'react/jsx-one-expression-per-line': 'off',
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
